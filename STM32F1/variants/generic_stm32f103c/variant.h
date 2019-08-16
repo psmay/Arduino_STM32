c5d95b9a2e6f75ed01473f0a0659a49cc433a016
Peter S. May <me@psmay.com>
2019-08-16 11:53:52 -0400

    Added `SERIAL_PORT_HARDWARE` and defs for `A0`-`A9` for `generic_stm32f103c` variant

--- STM32F1/variants/generic_stm32f103c/variant.h
+++ STM32F1/variants/generic_stm32f103c/variant.h
@@ -17,6 +17,31 @@ static const uint8_t MOSI = BOARD_SPI1_MOSI_PIN;
 static const uint8_t MISO = BOARD_SPI1_MISO_PIN;
 static const uint8_t SCK  = BOARD_SPI1_SCK_PIN;
 
+#define SERIAL_PORT_MONITOR         Serial
+#define SERIAL_PORT_HARDWARE        Serial1
+
 #define LED_BUILTIN PC13
+
+#define PIN_A0        20
+#define PIN_A1        21
+#define PIN_A2        22
+#define PIN_A3        23
+#define PIN_A4        24
+#define PIN_A5        25
+#define PIN_A6        26
+#define PIN_A7        27
+#define PIN_A8        28
+#define PIN_A9        29
+
+static const uint8_t A0 = PIN_A0;
+static const uint8_t A1 = PIN_A1;
+static const uint8_t A2 = PIN_A2;
+static const uint8_t A3 = PIN_A3;
+static const uint8_t A4 = PIN_A4;
+static const uint8_t A5 = PIN_A5;
+static const uint8_t A6 = PIN_A6;
+static const uint8_t A7 = PIN_A7;
+static const uint8_t A8 = PIN_A8;
+static const uint8_t A9 = PIN_A9;
 
 #endif /* _VARIANT_ARDUINO_STM32_ */
